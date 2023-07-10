ab6fd73f834b359aadb4ecf71aad8195e6d79b7d
AnirbanKM <anirbankreative22@gmail.com>
2023-07-11 00:20:56 +0530

    home page design

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const app = express();
 const ejs = require('ejs');
 const path = require('path');
 const expressLayout = require("express-ejs-layouts");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3300;
+
+// Assets
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
     res.render('home');

5b2a20ddff96398be25b7be54744c5ff28ff125f
AnirbanKM <anirbankreative22@gmail.com>
2023-07-13 02:09:26 +0530

    add to cart

---
+++ resources/js/app.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import Noty from 'noty';
+
+let addToCart = document.querySelectorAll('.add-to-cart');
+let cartCounter = document.querySelector("#cartCounter");
+
+function updateCart(pizza) {
+    axios.post('/update-cart', pizza).then((res) => {
+        cartCounter.innerText = res.data.totalQty;
+        new Noty({
+            text: "Item added to cart",
+            type: 'success',
+            closeWith: ['click', 'button'],
+            timeout: 1000
+        }).show();
+    }).catch((error) => {
+        new Noty({
+            text: "Something went wrong",
+            type: 'error',
+            closeWith: ['click', 'button'],
+            timeout: 1000
+        }).show();
+    });
+}
+
+addToCart.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+
+        let pizza = JSON.parse(btn.getAttribute('data-pizza'));
+        updateCart(pizza);
+    });
+});
--- server.js
+++ server.js
@@ -1,35 +1,63 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
 const path = require('path');
 const expressLayout = require("express-ejs-layouts");
+
 const PORT = process.env.PORT || 3300;
+const mongoose = require('mongoose');
+const session = require('express-session');
+const flash = require('express-flash');
+const MongoDbStore = require('connect-mongo');
+
+//  dotenv configuration
+dotenv.config();
+
+// Database Connection
+const url = "mongodb+srv://anirbandev:1234@cluster0.3jpx1eh.mongodb.net/pizza?retryWrites=true&w=majority";
+mongoose.connect(url).then(() => {
+    console.log("Connected to the database!");
+}).catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+});
+
+
+// Session store
+let mongoStore = MongoDbStore.create({
+    mongoUrl: url,
+    collection: 'session'
+})
+
+// Session config
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: mongoStore,
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hours
+}));
+
+
+app.use(flash());
 
 // Assets
 app.use(express.static('public'));
+app.use(express.json());
+
+//Global middleware
+app.use((req, res, next) => {
+    res.locals.session = req.session;
+    next();
+});
 
 // set Template engine
 app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
 
-
-app.get("/", (req, res) => {
-    res.render('home');
-});
-
-app.get("/cart", (req, res) => {
-    res.render('customers/cart');
-});
-
-app.get("/login", (req, res) => {
-    res.render('auth/login');
-});
-
-app.get("/register", (req, res) => {
-    res.render('auth/register');
-});
-
+require('./routes/web')(app);
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
